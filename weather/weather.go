6a5734f19550fe5d7d01f048b90e6ca12cfc90b7
Pradhuman Soni <soni.pradhuman@yahoo.in>
2021-10-29 10:57:44 +0530

    Changes

---
+++ weather/weather.go
@@ -0,0 +1,135 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
+	//"fyne.io/fyne/v2/widget"
+	"image/color"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	a := app.New()
+	w := a.NewWindow("Weather")
+
+	//w.Resize(fyne.Size{220, 330})
+
+	res, err := http.Get("http://api.openweathermap.org/data/2.5/weather?q=noida&appid=3a2dbe6d91cdd8b9cad883855f1666ed")
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	weather, err := UnmarshalWeather(body)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	img := canvas.NewImageFromFile("weather.png")
+	img.FillMode = canvas.ImageFillOriginal
+
+	label1 := canvas.NewText("Weather Details", color.White)
+	label1.TextStyle = fyne.TextStyle{Bold: true}
+
+	label2 := canvas.NewText(fmt.Sprintf("Country %s", weather.Sys.Country), color.White)
+	label3 := canvas.NewText(fmt.Sprintf("Wind Speed %.2f", weather.Wind.Speed), color.White)
+	label4 := canvas.NewText(fmt.Sprintf("Temperature %.2f", (weather.Main.Temp - 273.15)), color.White)
+	label5 := canvas.NewText(fmt.Sprintf("Humidity %2d", weather.Main.Humidity), color.White)
+
+	w.SetContent(
+		container.NewVBox(
+			img,
+			label1,
+			label2,
+			label3,
+			label4,
+			label5,
+		),
+	)
+	w.ShowAndRun()
+}
+
+// This file was generated from JSON Schema using quicktype, do not modify it directly.
+// To parse and unparse this JSON data, add this code to your project and do:
+//
+//    weather, err := UnmarshalWeather(bytes)
+//    bytes, err = weather.Marshal()
+
+func UnmarshalWeather(data []byte) (Weather, error) {
+	var r Weather
+	err := json.Unmarshal(data, &r)
+	return r, err
+}
+
+func (r *Weather) Marshal() ([]byte, error) {
+	return json.Marshal(r)
+}
+
+type Weather struct {
+	Coord      Coord            `json:"coord"`
+	Weather    []WeatherElement `json:"weather"`
+	Base       string           `json:"base"`
+	Main       Main             `json:"main"`
+	Visibility int64            `json:"visibility"`
+	Wind       Wind             `json:"wind"`
+	Clouds     Clouds           `json:"clouds"`
+	Dt         int64            `json:"dt"`
+	Sys        Sys              `json:"sys"`
+	Timezone   int64            `json:"timezone"`
+	ID         int64            `json:"id"`
+	Name       string           `json:"name"`
+	Cod        int64            `json:"cod"`
+}
+
+type Clouds struct {
+	All int64 `json:"all"`
+}
+
+type Coord struct {
+	Lon float64 `json:"lon"`
+	Lat float64 `json:"lat"`
+}
+
+type Main struct {
+	Temp      float64 `json:"temp"`
+	FeelsLike float64 `json:"feels_like"`
+	TempMin   float64 `json:"temp_min"`
+	TempMax   float64 `json:"temp_max"`
+	Pressure  int64   `json:"pressure"`
+	Humidity  int64   `json:"humidity"`
+	SeaLevel  int64   `json:"sea_level"`
+	GrndLevel int64   `json:"grnd_level"`
+}
+
+type Sys struct {
+	Type    int64  `json:"type"`
+	ID      int64  `json:"id"`
+	Country string `json:"country"`
+	Sunrise int64  `json:"sunrise"`
+	Sunset  int64  `json:"sunset"`
+}
+
+type WeatherElement struct {
+	ID          int64  `json:"id"`
+	Main        string `json:"main"`
+	Description string `json:"description"`
+	Icon        string `json:"icon"`
+}
+
+type Wind struct {
+	Speed float64 `json:"speed"`
+	Deg   int64   `json:"deg"`
+	Gust  float64 `json:"gust"`
+}
